0131c23052490353165ae7d4565d25269f04560c
Fan Guo <guofan1995@gmail.com>
2023-09-14 18:19:12 -0400

    155. Min Stack

--- week4/min-stack.rb
+++ week4/min-stack.rb
@@ -55,3 +55,22 @@ end
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.get_min()
+
+# Me again, so there's a few different ways of doing this. Once of
+# them is what I did, another one is to track the current min with
+# each value that's pushed onto the stack
+# e.g. for values [3, 2, 1, 5, 6, 0] that get pushed, we'd store
+# [[3, 3], [2, 2], [1, 1], [5, 1], [6, 1], [0, 0]] i.e. [val, min].
+#
+# Downside to this and the one I implemented is that if we added the
+# same min value multiple times, there's a lot of repetition and
+# wasted space.
+#
+# So alternatively, we could adapt what I already had and instead of
+# continuously pushing the same min value if it occurs, we track two
+# values in the min_history stack, which is the min value and how
+# many times it's appeared, and when we pop, we compare both the min
+# value and its occurrences before removing it from the min_his stack
+# e.g. for [1, 2, 3, 1, 1, 0, 2, 0], we'd have
+# stack: [1, 2, 3, 1, 1, 0, 2, 0]
+# min_history: [[1, 3], [0,2]] instead of [1, 1, 1, 0, 0]
